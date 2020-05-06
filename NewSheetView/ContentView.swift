47d7091496892f621260c79719cfbc06133ad4c7
heroparkjisung2 <heroparkjisung2@gmail.com>
2020-05-06 10:39:41 +0900

    Initial Commit

---
+++ NewSheetView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewSheetView
+//
+//  Created by JU HAN LEE on 2020/05/06.
+//  Copyright © 2020 yht. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
